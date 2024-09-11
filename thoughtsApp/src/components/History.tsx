2c9566f05c0c32b2753cf306deee3b5a2c7b7e58
kaya70875 <shortsforturkey@gmail.com>
2024-09-11 20:48:18 +0300

    adding history section

--- thoughtsApp/src/components/History.tsx
+++ thoughtsApp/src/components/History.tsx
@@ -2,13 +2,35 @@ import { useForm } from "../context/FormContext";
 import HistoryCard from "./alts/HistoryCard";
 
 export default function History() {
-  const { submittedForms } = useForm();
-  console.log(submittedForms)
+  const { submittedForms, setSubmittedForms } = useForm();
+
+  const handleRemove = (index: number) => {
+    setSubmittedForms(prevForms => prevForms.filter((_, i) => i !== index));
+  }
+
   return (
-    <div className="w-2/4 h-full flex flex-col items-center gap-6 mt-36">
-      {submittedForms.map((form, index) => (
-        <HistoryCard form={form} key={index}/>
-      ))} 
+    <div>
+      {submittedForms.length > 0 && submittedForms.map((form, index) => (
+        <div key={index} className="w-full mt-24 ml-48 flex flex-col gap-6">
+          <div className="flex justify-between items-center">
+            <p className="text-textColor">
+              {new Date().toLocaleString("en-US", {
+                weekday: "long",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+                hour12: true,
+              })}
+            </p>
+            <button onClick={() => handleRemove(index)} className="ml-2 text-red-500">x</button>
+          </div>
+
+          <HistoryCard form={form} />
+        </div>
+      ))}
     </div>
-  )
+  );
 }
