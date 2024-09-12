af95e96860ce84f37345df2fd5cbfbd19be7d875
kaya70875 <shortsforturkey@gmail.com>
2024-09-12 17:44:42 +0300

    add sticky scrolling to history cards

--- thoughtsApp/src/App.tsx
+++ thoughtsApp/src/App.tsx
@@ -3,13 +3,16 @@ import Editor from './components/Editor'
 import History from './components/History'
 import { FormProvider } from './context/FormContext'
 import './index.css'
+import '../src/css/ScrollBar.css'
 
 function App() {
   return (
     <FormProvider>
-      <div className="bg-background w-screen h-screen flex">
+      <div className="bg-background w-screen h-screen flex overflow-hidden">
         <Editor />
-        <History />
+        <div className='flex-1 overflow-auto overflow-x-hidden smooth-scroll'>
+          <History />
+        </div>
       </div>
     </FormProvider>
     
--- thoughtsApp/src/components/History.tsx
+++ thoughtsApp/src/components/History.tsx
@@ -2,7 +2,7 @@ import { useForm } from "../context/FormContext";
 import HistoryCard from "./alts/HistoryCard";
 
 export default function History() {
-  const { submittedForms, setSubmittedForms , form} = useForm();
+  const { submittedForms, setSubmittedForms} = useForm();
 
   const handleRemove = (index: number) => {
     setSubmittedForms(prevForms => prevForms.filter((_, i) => i !== index));
@@ -10,12 +10,12 @@ export default function History() {
   }
 
   return (
-    <div className="w-5/12 sticky">
+    <div className="w-9/12 sticky top-0 p-6 scroll-smooth">
       {submittedForms.length > 0 && submittedForms.map((form, index) => (
         <div key={index} className="w-full mt-24 ml-48 flex flex-col gap-6">
           <div className="flex justify-between items-center">
             <p>{form.date}</p>
-            <button onClick={() => handleRemove(index)} className="ml-2 text-red-500">x</button>
+            <button onClick={() => handleRemove(index)} className="ml-2">x</button>
           </div>
 
           <HistoryCard form={form} />
