2a263d4fd21c1be5cbe290007ae214c7a4a75a20
kaya70875 <shortsforturkey@gmail.com>
2024-09-14 17:03:40 +0300

    adding no history section

--- thoughtsApp/src/components/History.tsx
+++ thoughtsApp/src/components/History.tsx
@@ -11,7 +11,7 @@ export default function History() {
 
   return (
     <div className="w-9/12 sticky top-0 p-6 scroll-smooth">
-      {submittedForms.length > 0 && submittedForms.map((form, index) => (
+      {submittedForms.length > 0 ? (submittedForms.map((form, index) => (
         <div key={index} className="w-full mt-24 ml-48 flex flex-col gap-6">
           <div className="flex justify-between items-center">
             <p>{form.date}</p>
@@ -20,7 +20,12 @@ export default function History() {
 
           <HistoryCard form={form} />
         </div>
-      ))}
+      ))) : (
+        <div className="flex flex-col justify-center items-center h-screen ml-48 gap-6">
+          <h1 className="font-bold text-textColor text-lg">No History Yet</h1>
+          <p className="text-center text-base">Record your thoughts and mood for reflection. Everything is stored locally in your browser. Export data when you need it.</p>
+        </div>
+      )}
     </div>
   );
 }
